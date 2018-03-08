a399fcf9a7c8ea4b71345bf99458c925ee632f90
wangbangjun <771854332@qq.com>
2018-03-08 21:24:54 +0800

    fix: 依赖错误，恢复react-router

--- client/server-entry.js
+++ client/server-entry.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StaticRouter } from 'dva/router';
+import { StaticRouter } from 'react-router';
 import { Provider } from 'react-redux';
 import create from './views/App';
 
